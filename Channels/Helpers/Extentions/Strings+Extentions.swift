174ee4e1b451c4cdea0e42d29efe693581567dcb
ayaemahmoud <ayae.mahmoud@gmial.com>
2020-04-07 09:52:11 +0200

    Merge

---
+++ Channels/Helpers/Extentions/Strings+Extentions.swift
@@ -0,0 +1,9 @@
+//
+//  Strings+Extentions.swift
+//  Channels
+//
+//  Created by Aya E  Mahmoud on 3/24/20.
+//  Copyright © 2020 Aya E  Mahmoud. All rights reserved.
+//
+
+import Foundation
---
+++ Channels/Models/ResponseModels/SampleResponseModel.swift
@@ -0,0 +1,9 @@
+//
+//  SampleResponseModel.swift
+//  Channels
+//
+//  Created by Aya E  Mahmoud on 4/6/20.
+//  Copyright © 2020 Aya E  Mahmoud. All rights reserved.
+//
+
+import Foundation
